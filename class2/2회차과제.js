dd638235751f909b3595b3f7be86e38bc00cca13
Jin <1123kimhj@netive.co.kr>
2017-04-25 13:50:18 +0900

    2회차

---
+++ class2/2회차과제.js
@@ -0,0 +1,15 @@
+// 문제 1.
+// 다음 배열의 평균값을 구하는 함수를 만들어 보세요. 배열의 어떤값이 와도 평균값을 구할 수 있어야 합니다.
+var numArray = [3,4,5];
+function numAverage(){
+
+}
+console.log("평균값 : " + numAverage(numArray));
+
+// 문제 2.
+// 사용자에게 가위,바위,보 중 하나를 입력받고 컴퓨터의 가위바위보와 비교하여 결과를 나타내는 함수를 만들어 보세요.
+function playGame(){
+
+}
+
+console.log("당신은 "+ playGame());
